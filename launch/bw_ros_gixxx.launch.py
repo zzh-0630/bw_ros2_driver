6583674da9d2fc392b3cff6976a2f5b115e235c9
zzh-0630 <1356247257@qq.com>
2026-04-15 10:36:50 +0800

    feat(launch): add gixxx launch file

---
+++ launch/bw_ros_gixxx.launch.py
@@ -0,0 +1,20 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('bw_ros2_driver')
+    params_file = os.path.join(pkg_share, 'config', 'gixxx_params_ros2.yaml')
+
+    return LaunchDescription([
+        Node(
+            package='bw_ros2_driver',
+            executable='bw_node_gixxx',
+            name='bw_node_gixxx',
+            output='screen',
+            parameters=[params_file],
+        ),
+    ])
